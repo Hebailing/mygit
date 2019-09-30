c9d10fbb8bca94b33adbbe520d267251be8d6e6c
hebailing <891189901@qq.com>
2019-09-30 11:29:58 +0800

    test

---
+++ src/test/java/com/cnblue/xsy/XsyApplicationTests.java
@@ -0,0 +1,16 @@
+package com.cnblue.xsy;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class XsyApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
